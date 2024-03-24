5e7f35f328f04702544b4692779d9f53ddf22661
Bhushan Gayakwad <bg828587@gmail.com>
2024-03-24 10:10:45 +0530

    compilation & dart variable

---
+++ 1_variable.dart
@@ -0,0 +1,42 @@
+void main(){
+  String name = "Bhushan";
+  String msg = 'A';
+  var text = "hello";
+  var txt = 'H';
+
+  int num1 = 12;
+  double num2 = 23.54;
+  var numb = 12;
+  var number = 23.54;
+
+  bool isAdmin = true;
+  var isMarried = true;
+
+  num anyNum_1 = 12;    // -> both float/double/decimal and int
+  num anyNum_2 = 24.46;
+
+
+  //constants
+  const double pi_1 = 3.14;
+  const pi_2 = 3.14; //can write without specify the data type, it will automatically assign by detecting value
+  const COLOR = "Blue";
+  // COLOR = "Red";  not possible
+
+
+
+  // num -> sealed class implement Comparable<num>
+  // (abstract final class) int and double -> extends num
+  // (abstract final class) String ->  implement Comparable<String>, Pattern 
+  // (final class) bool,
+  // var, const
+  // var can store any type value, but i am seeing const also store any type value, 
+
+
+
+
+
+
+
+
+
+}
--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
 void main(){
   print("welcome to dart tutorial");
 }
+
+
+// NOTE ->
+/*
+1. dart2js compiler -> dart to js, so that it can also run on browser
+   command-> dart compile js main.dart
+   result-> out.js, out.hs.deps, out.js.map, 
+   now run out.js like normal js file-> command->  node out.js  -> o/p will same as for dart file
+
+2. Compile Dart direclty-> 
+    command-> dart compile exe main.dart
+ */
+
